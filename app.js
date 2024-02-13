4dc337da9b0fe9ccd611cb5f87e8f466b8d3fddf
Aliaskar Uteshkaliev <121083865+Oskr11@users.noreply.github.com>
2024-02-14 03:00:33 +0600

    ENV PORT app.js

--- app.js
+++ app.js
@@ -227,9 +227,12 @@ app.post("/", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server started on 3000");
+const port = process.env.PORT || 3000; 
+
+app.listen(port, function () {
+    console.log(`Server started on ${port}`);
 });
+
 
 
 
